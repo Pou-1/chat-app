cf588f17493bdea8c64a87f4db74dc59a3699486
cyprien <cyprien.delapoeze-dharambure@etu.unicaen.fr>
2024-01-25 14:11:01 +0100

    Vue js project

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import ChatViewVue from '@/views/ChatView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'chat',
+      component: ChatViewVue
+    },
+  ]
+})
+
+export default router
